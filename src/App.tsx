460fa986420a38049f5174d650524d472f1f5a87
Aldo Tobing <aldo_tobing@hotmail.com>
2024-12-18 12:32:26 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Helmet } from "react-helmet";
+// import logo from "./logo.svg";
 import "./App.css";
+import ChatInterface from "./ChatInterface"; // Import ChatInterface
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Helmet>
+        <title>AI Chat Assistant | Kapal Lawd</title>
+        <meta
+          name="description"
+          content="Kapal Lawd AI Chat Assistant - An intelligent AI assistant. Capable of answering questions, providing information, and generating images on demand. developed by Aldo Tobing"
+        />
+        <meta
+          name="keywords"
+          content="AI, Chat, Assistant, Kapal Lawd, Weather, Aldo Tobing, Java, Golang, PostgreSQL, DevOps, Software Engineer, Docker, Backend Developer"
+        />
+      </Helmet>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +31,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <ChatInterface /> {/* Render ChatInterface here */}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ChatInterface from "./ChatInterface";
+// import ChatInterface from "./ChatInterface";
+import App from "./App";
 import "./index.css";
 import "./assets/code.css";
 import { register } from "./ServiceWorker";
@@ -11,7 +12,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <ChatInterface />
+    <App />
   </React.StrictMode>
 );
 
